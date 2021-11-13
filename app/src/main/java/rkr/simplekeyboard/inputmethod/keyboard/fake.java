89233217d2f23270d616c66cb628f69dff24f5c1
Nafi-13 <nafirazzaque@gmail.com>
2021-11-13 22:11:50 +0600

    fake addition

---
+++ app/src/main/java/rkr/simplekeyboard/inputmethod/keyboard/fake.java
@@ -0,0 +1,18 @@
+package rkr.simplekeyboard.inputmethod.keyboard;
+
+import android.util.SparseArray;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import rkr.simplekeyboard.inputmethod.keyboard.internal.KeyVisualAttributes;
+import rkr.simplekeyboard.inputmethod.keyboard.internal.KeyboardIconsSet;
+import rkr.simplekeyboard.inputmethod.keyboard.internal.KeyboardParams;
+import rkr.simplekeyboard.inputmethod.latin.common.Constants;
+
+public class fake {
+
+
+
+}
